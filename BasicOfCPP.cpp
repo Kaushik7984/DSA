7dedaa9755498d1b7b1bcbc844512b8c613a0bb8
Kaushik7984 <kaushik7984@gmail.com>
2024-06-24 22:02:28 +0530

    initialize commit

---
+++ BasicOfCPP.cpp
@@ -0,0 +1,41 @@
+#include<iostream> // cout is belongs to this library
+using namespace std;  // to use cout
+
+int main() {
+    
+  cout << " HEllO WORLD " << endl;  // endl for new line
+  cout << " \n I'm Kaushik Tapaniya" ; // cout << for print output on display
+
+  int a = 123;
+  cout<< a <<endl;
+
+  char b = 'c';
+  cout<< b <<endl;
+
+  bool bl = true;
+  cout<< bl <<endl;
+
+  float f = 1.22;
+  cout<< f <<endl;
+
+  double d =1.555;
+  cout<< d <<endl;
+
+  int size = sizeof(d); // for count size of datatype
+  cout<< "Size of d is:" <<size <<endl <<endl;
+
+
+  //type Casting
+
+  int t = 'a';
+  cout<<  t <<endl;
+
+  char ch1 = 97;
+  cout<< ch1 <<endl;
+
+  char ch2 = 123456;
+  cout<< ch2 <<endl;
+
+
+}
+
